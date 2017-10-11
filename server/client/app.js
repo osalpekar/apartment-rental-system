6b0efbfd2bbf0b4fa6f17401cd44257fbc903142
Omkar Salpekar <osalpekar@berkeley.edu>
2017-10-11 01:57:59 -0700

    added angularJS routing for newly added endpoints

--- server/client/app.js
+++ server/client/app.js
@@ -5,6 +5,14 @@ app.config(['$routeProvider', function ($routeProvider) {
         controller: 'tenantController',
         template: 'views/tenants.html'
     })
+    .when('/app/psql/users', {
+        controller: 'tenantController',
+        template: 'views/tenants.html'
+    })
+    .when('/app/mysql/users', {
+        controller: 'tenantController',
+        template: 'views/tenants.html'
+    })
     .otherwise({
         redirectTo: '/app/users'
     });
